e2e4e605c458f4eea83705fee939bd36fb50ea2e
James Robb <47126579+jamesrweb@users.noreply.github.com>
2021-03-15 02:17:53 +0100

    Reducing CRAP score in coverage report

--- src/Client.php
+++ src/Client.php
@@ -9,6 +9,7 @@ use CodewarsKataExporter\Interfaces\ClientOptionsInterface;
 use Nyholm\Psr7\Factory\Psr17Factory;
 use Psr\Http\Client\ClientInterface as Psr18ClientInterface;
 use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Symfony\Component\HttpClient\Psr18Client;
 
@@ -16,28 +17,28 @@ final class Client implements ClientInterface
 {
     private Psr18ClientInterface $psr18Client;
     private RequestFactoryInterface $psr17Factory;
-    private ClientOptionsInterface $request_options;
-    private string $base_url = 'https://www.codewars.com/api/v1';
+    private ClientOptionsInterface $client_options;
+    private string $base_uri = 'https://www.codewars.com/api/v1';
 
     public function __construct(ClientOptionsInterface $options)
     {
-        $this->request_options = $options;
+        $this->client_options = $options;
         $this->psr18Client = new Psr18Client();
         $this->psr17Factory = new Psr17Factory();
     }
 
     public function user(string $username): array
     {
-        $url = "{$this->base_url}/users/{$username}";
-        $response = $this->request('GET', $url);
+        $uri = "{$this->base_uri}/users/{$username}";
+        $response = $this->request('GET', $uri);
 
         return $this->parse($response);
     }
 
     public function authored(string $username): array
     {
-        $url = "{$this->base_url}/users/{$username}/code-challenges/authored";
-        $response = $this->request('GET', $url);
+        $uri = "{$this->base_uri}/users/{$username}/code-challenges/authored";
+        $response = $this->request('GET', $uri);
         $body = $this->parse($response);
 
         return $body['data'];
@@ -51,7 +52,8 @@ final class Client implements ClientInterface
     private function completedPaginationHelper(string $username, int $page = 1, array $output = []): array
     {
         $query = http_build_query(['page' => $page - 1]);
-        $response = $this->request('GET', "{$this->base_url}/users/{$username}/code-challenges/completed?{$query}");
+        $uri = "{$this->base_uri}/users/{$username}/code-challenges/completed?{$query}";
+        $response = $this->request('GET', $uri);
         $body = $this->parse($response);
         $output = array_merge($output, $body['data']);
 
@@ -64,7 +66,8 @@ final class Client implements ClientInterface
 
     public function challenge(string $id): array
     {
-        $response = $this->request('GET', "{$this->base_url}/code-challenges/{$id}");
+        $uri = "{$this->base_uri}/code-challenges/{$id}";
+        $response = $this->request('GET', $uri);
 
         return $this->parse($response);
     }
@@ -73,20 +76,25 @@ final class Client implements ClientInterface
     {
         return array_reduce($challenges, function (array $accumulator, array $current): array {
             $id = $current['id'];
-            $response = $this->request('GET', "{$this->base_url}/code-challenges/{$id}");
+            $uri = "{$this->base_uri}/code-challenges/{$id}";
+            $response = $this->request('GET', $uri);
             $accumulator[] = $this->parse($response);
 
             return $accumulator;
         }, []);
     }
 
-    private function request(string $method, string $url): ResponseInterface
+    private function request(string $method, string $uri): ResponseInterface
     {
-        $request = $this->psr17Factory->createRequest($method, $url);
+        $headers = $this->client_options->headers();
 
-        foreach ($this->request_options->headers() as $header => $value) {
-            $request = $request->withHeader($header, $value);
-        }
+        $request = array_reduce(
+            array_keys($headers),
+            function (RequestInterface $accumulator, string $current) use ($headers): RequestInterface {
+                return $accumulator->withHeader($current, $headers[$current]);
+            },
+            $this->psr17Factory->createRequest($method, $uri)
+        );
 
         return $this->psr18Client->sendRequest($request);
     }
